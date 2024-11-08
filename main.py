f31bae518dfe617e349b5095280e372fdf41e151
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2024-11-07 20:22:29 -0500

    Basic Adding Shader

---
+++ main.py
@@ -0,0 +1,51 @@
+from kp import Manager, Tensor, Algorithm, OpBase, OpAlgoDispatch, OpTensorSyncLocal, OpTensorSyncDevice, OpTensorSyncLocal
+from itertools import chain
+
+
+def chain_lists(*args):
+    return list(chain.from_iterable(args))
+
+def get_shader(name):
+    with open("./shaders/{}.spv".format(name), "rb") as file:
+        return file.read()
+
+def get_alg(mgr, data, shader_name):
+    return mgr.algorithm(tensors=data, spirv=get_shader(shader_name))
+
+def run_algorithm(mgr, data, shader_name):
+    # Sequence of operations. However, if written properly, this can be done in 1 operation
+    sq = mgr.sequence()
+   
+    output = mgr.tensor([0 for i in range(len(data[0]))])
+    # I want to make my life easier, so all the data goes in as a list of lists
+    data = [mgr.tensor(d) for d in data] # MAKE THIS WORK PROPERLY BY SETTING TYPE TO INT32
+   
+    mem = chain_lists(data, [output])
+    
+    # Sync data from RAM to device
+    sq.eval(OpTensorSyncDevice(mem))
+    
+    # Setup alg
+    alg = get_alg(mgr, mem, shader_name)
+    
+    # Send alg to device and run it
+    sq.eval(OpAlgoDispatch(alg))
+
+    # Sync data from device to RAM
+    sq.eval(OpTensorSyncLocal([output]))
+
+    return output
+
+# Initialize Kompute Manager
+mgr = Manager()
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+res = run_algorithm(mgr, [a, b], "adding")
+
+# Output the result
+print("Result:", res.data())
+
+# Clean up resources
+mgr.destroy()
