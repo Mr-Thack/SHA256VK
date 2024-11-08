75e7ed4e54fad3c6ebc19d3b4b3cd7e6d1119350
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2024-11-07 22:11:49 -0500

    Not working, but WIP

--- gpu.py
+++ gpu.py
@@ -1,27 +1,42 @@
 from kp import Manager, Tensor, Algorithm, OpBase, OpAlgoDispatch, OpTensorSyncLocal, OpTensorSyncDevice, OpTensorSyncLocal
 from itertools import chain
-
+from numpy import int32, uint32, array
 
 def chain_lists(*args):
     return list(chain.from_iterable(args))
 
 def get_shader(name):
-    with open("./shaders/{}.spv".format(name), "rb") as file:
+    with open("./{}.spv".format(name), "rb") as file:
         return file.read()
 
 def get_alg(mgr, data, shader_name):
     return mgr.algorithm(tensors=data, spirv=get_shader(shader_name))
 
-def run_algorithm(mgr, data, shader_name):
+def arr(mgr, data, dtype):
+    return mgr.tensor(array(data, dtype=dtype))
+
+def run_algorithm(mgr, data, output_len, shader_name, dtype):
     # Sequence of operations. However, if written properly, this can be done in 1 operation
     sq = mgr.sequence()
    
-    output = mgr.tensor([0 for i in range(len(data[0]))])
+    if dtype == "uint32":
+        dtype = uint32
+    elif dtype == "int32":
+        dtype = int32
+    else:
+        print("Somethign is wrong with data type")
+        quit()
+
+    # Not the most efficient way to do it, but screewwwww it
+    # This is just a draft
+    output = arr(mgr, [0 for i in range(output_len)], dtype)
     # I want to make my life easier, so all the data goes in as a list of lists
-    data = [mgr.tensor(d) for d in data] # MAKE THIS WORK PROPERLY BY SETTING TYPE TO INT32
+    data = [arr(mgr, d, dtype) for d in data] # MAKE THIS WORK PROPERLY BY SETTING TYPE TO INT32
    
     mem = chain_lists(data, [output])
-    
+   
+    for tensor in mem:
+        print(tensor.data())
     # Sync data from RAM to device
     sq.eval(OpTensorSyncDevice(mem))
     
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from gpu import make_manager, run_algorithm
+import hashlib
+
+def compare_sha256 ( res : bytes, data : bytes ) -> bool:
+    """
+    Compares a given SHA - 256 hash(res)with the hash computed using hashlib for the same data .
+
+    Parameters :
+    res(bytes): The SHA - 256 hash result to be compared .
+    data(bytes): The original data to hash for comparison .
+
+    Returns :
+    bool : True if the hashes match, False otherwise .
+    """
+    # Compute the SHA-256 hash using hashlib
+    expected_hash = hashlib . sha256(data). digest()
+    # Compare and return the result
+    return res == expected_hash
 
 # Initialize Kompute Manager
 mgr = make_manager()
 
-a = [1, 2, 3]
-b = [4, 5, 6]
+a = [ 1, 2, 3 ]
+b = [ 4, 5, 6 ]
 
-res = run_algorithm(mgr, [a, b], "adding")
+res = run_algorithm(mgr, [a, b], 3, "adding", "uint32")
 
 # Output the result
 print("Result:", res.data())
