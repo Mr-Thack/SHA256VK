d1a61a6e91043ef47e3ab751bb2b5b98c112d12b
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2024-11-21 15:41:29 -0500

    Not exactly working, but using default GLSL shader from GLHash; Will rewrite to use custom shader later

--- main.cpp
+++ main.cpp
@@ -13,41 +13,70 @@
 #include <kompute/Tensor.hpp>
 #include <vector>
 
+using std::shared_ptr;
 using std::string;
 using std::vector;
 
-string capitalize(const string str) {
-  if (str.empty())
-    return str; // Handle empty strings
-  string result = str;
-  result[0] = std::toupper(result[0]); // Capitalize first character
-  return result;
-}
+// I'm getting bored of typing out std::shared_ptr<kp::TensorT<float>>, so:
+typedef shared_ptr<kp::TensorT<uint32_t>> UTens;
+typedef vector<uint32_t> uvec;
 
 fmt::text_style FMTFLAGS = fmt::emphasis::bold | fg(fmt::color::green);
 
-void printObj(string name, vector<uint32_t> data) {
-  fmt::print(FMTFLAGS, "{} {{ {} }}\n", capitalize(name),
-             fmt::join(data, ", "));
+void insert(uvec &res, const uvec &v) {
+  res.insert(res.end(), v.begin(), v.end());
 }
 
-void printObj(string name, std::shared_ptr<kp::TensorT<uint32_t>> data) {
-  printObj(name, data->vector());
-}
-
-void printObj(string name, std::vector<std::vector<uint32_t>> data) {
-  fmt::print(FMTFLAGS, "{} {{\n", capitalize(name));
-  for (const auto &iv : data) {
-    printObj("", iv);
+string chain_to_hex(const vector<uint32_t> &words) {
+  std::stringstream ss;
+  ss << std::hex << std::setfill('0');
+  for (uint32_t word : words) {
+    ss << std::setw(8) << word;
   }
-  fmt::print(FMTFLAGS, "}}\n");
+  return ss.str();
 }
 
-// Macro to print a vector with the capitalized variable name
-#define printo(data) printObj(#data, data);
+int main() {
+  kp::Manager mgr;
 
-// I'm getting bored of typing out std::shared_ptr<kp::TensorT<float>>, so:
-typedef std::shared_ptr<kp::TensorT<uint32_t>> UTens;
-typedef std::vector<uint32_t> uvec;
+  string input_text = "Hello!";
 
-int main() { kp::Manager mgr; }
+  uvec chain(8);
+  uvec block(16);
+  uint32_t offset = 0;
+  uint32_t length = input_text.size() * 8;
+  uvec input_data(input_text.begin(), input_text.end());
+
+  uvec data;
+  data.reserve(8 + 16 + 1 + 1 + input_data.size());
+
+  insert(data, chain);
+  insert(data, block);
+  data.push_back(offset);
+  data.push_back(length);
+  insert(data, input_data);
+
+  UTens datat = mgr.tensorT(data);
+
+  const vector<shared_ptr<kp::Memory>> params = {datat};
+
+  const vector<uint32_t> shader = vector<uint32_t>(shader::ALG_COMP_SPV.begin(),
+                                                   shader::ALG_COMP_SPV.end());
+
+  kp::Workgroup workgroup({1, 1, 1});
+  shared_ptr<kp::Algorithm> algo = mgr.algorithm(params, shader, workgroup);
+  mgr.sequence()
+      ->record<kp::OpSyncDevice>(params)
+      ->record<kp::OpAlgoDispatch>(algo)
+      ->record<kp::OpSyncLocal>(params)
+      ->eval();
+
+  uvec data_result = datat->vector();
+  uvec rchain(data_result.begin(), data_result.begin() + 8);
+  for (auto &it : rchain) {
+    fmt::print("{} ", it);
+  }
+  fmt::print("\n");
+  fmt::print(FMTFLAGS, chain_to_hex(rchain));
+  return 0;
+}
